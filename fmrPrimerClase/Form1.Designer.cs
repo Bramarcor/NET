2ad1cb752d30b688c0ccd99284539ac2ba9662f7
Bramarcor <bramarcor@gmail.com>
2019-05-14 21:12:01 -0300

    Realizamos el Calculo

--- fmrPrimerClase/Form1.Designer.cs
+++ fmrPrimerClase/Form1.Designer.cs
@@ -28,66 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.lblNumeroUno = new System.Windows.Forms.Label();
+            this.lblNumerDos = new System.Windows.Forms.Label();
+            this.txtNumeroUno = new System.Windows.Forms.TextBox();
+            this.txtNumeroDos = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // label1
+            // lblNumeroUno
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(19, 21);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Numero 1";
+            this.lblNumeroUno.AutoSize = true;
+            this.lblNumeroUno.Location = new System.Drawing.Point(19, 15);
+            this.lblNumeroUno.Name = "lblNumeroUno";
+            this.lblNumeroUno.Size = new System.Drawing.Size(53, 13);
+            this.lblNumeroUno.TabIndex = 0;
+            this.lblNumeroUno.Text = "Numero 1";
             // 
-            // label2
+            // lblNumerDos
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(19, 48);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(53, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Numero 2";
+            this.lblNumerDos.AutoSize = true;
+            this.lblNumerDos.Location = new System.Drawing.Point(19, 42);
+            this.lblNumerDos.Name = "lblNumerDos";
+            this.lblNumerDos.Size = new System.Drawing.Size(53, 13);
+            this.lblNumerDos.TabIndex = 1;
+            this.lblNumerDos.Text = "Numero 2";
             // 
-            // textBox1
+            // txtNumeroUno
             // 
-            this.textBox1.Location = new System.Drawing.Point(76, 18);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 2;
+            this.txtNumeroUno.Location = new System.Drawing.Point(76, 12);
+            this.txtNumeroUno.Name = "txtNumeroUno";
+            this.txtNumeroUno.Size = new System.Drawing.Size(100, 20);
+            this.txtNumeroUno.TabIndex = 2;
             // 
-            // textBox2
+            // txtNumeroDos
             // 
-            this.textBox2.Location = new System.Drawing.Point(76, 45);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 3;
+            this.txtNumeroDos.Location = new System.Drawing.Point(76, 39);
+            this.txtNumeroDos.Name = "txtNumeroDos";
+            this.txtNumeroDos.Size = new System.Drawing.Size(100, 20);
+            this.txtNumeroDos.TabIndex = 3;
             // 
-            // button1
+            // btnCalcular
             // 
-            this.button1.Location = new System.Drawing.Point(101, 105);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnCalcular.Location = new System.Drawing.Point(76, 104);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(100, 38);
+            this.btnCalcular.TabIndex = 4;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(196, 140);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(197, 172);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtNumeroDos);
+            this.Controls.Add(this.txtNumeroUno);
+            this.Controls.Add(this.lblNumerDos);
+            this.Controls.Add(this.lblNumeroUno);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -95,11 +97,11 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblNumeroUno;
+        private System.Windows.Forms.Label lblNumerDos;
+        private System.Windows.Forms.TextBox txtNumeroUno;
+        private System.Windows.Forms.TextBox txtNumeroDos;
+        private System.Windows.Forms.Button btnCalcular;
     }
 }
 
--- fmrPrimerClase/Form1.cs
+++ fmrPrimerClase/Form1.cs
@@ -12,9 +12,31 @@ namespace fmrPrimerClase
 {
     public partial class Form1 : Form
     {
+        private int valorCalculado = 0;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            valorCalculado = Convert.ToInt32(this.txtNumeroUno.Text) + Convert.ToInt32(this.txtNumeroDos.Text);
+
+            // El Sigo $ Permite junto con las llaves mostrar el valor de una variable
+            // Sin el signo no lo permite
+
+            //MessageBox.Show("La Suma Es: " + valorCalculado);
+
+            MessageBox.Show($"La Suma Es: {valorCalculado}");
+        }
+
     }
 }
