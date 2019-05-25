dfec50561c9e4ee46eaa7f5b36ebe6928b774ecc
Bramarcor <bramarcor@gmail.com>
2019-05-24 21:57:33 -0300

    Cambios Clase 4

--- OOP2/Alumno.cs
+++ OOP2/Alumno.cs
@@ -10,10 +10,21 @@ namespace OOP2
 {
     class Alumno : Persona
     {
+        private double nota;
 
-        double nota;
+        private string valor;
 
-        
+        // Lo siguiente se llama Property
+        // se usa como si fuera un metodo para cambiar los valores de las variables privadas
+        // el siguiente tiene el nombre Nota y se pueden utilizar para validar los valores que se obtienen o ingresan
+
+        public string Nota
+        {
+            get { return this.nota.ToString(); } // retorna el valor de la variable nota convertida en string
+            set { valor = value; } // lo que se le asigne a Nota se le va a asignar a valor
+        }
+
+
         public Alumno(double nota, string nombre, string apellido, string materia) : base(nombre, apellido, materia)
         {
             this.nota = nota;
@@ -28,6 +39,9 @@ namespace OOP2
         }
         */
         
+
+
+       
           
        public override string ToString()
        {
