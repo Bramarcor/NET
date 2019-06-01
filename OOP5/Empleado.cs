a685bdd231a16e72c18ac79bc18b5ed72d1ce8cd
Bramarcor <bramarcor@gmail.com>
2019-06-01 17:37:19 -0300

    Cambios

---
+++ OOP5/Empleado.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP5
+{
+    class Empleado
+    {
+        protected string nombreEmpleado;
+        protected double sueldoDepositado;
+
+        public Empleado(string nombre, double deposito)
+        {
+            this.nombreEmpleado = nombre;
+            this.sueldoDepositado = deposito;
+        }
+
+        public double obtenerSueldo()
+        {
+            return this.sueldoDepositado;
+        }
+
+        }
+    }
+}
